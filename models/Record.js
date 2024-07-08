e58e960a67358ea1b4ba008bf9f97e063722a62d
Jayendra Vardhan Singh <jayendravardhan@gmail.com>
2024-07-08 15:41:30 +0530

    first commit

---
+++ models/Record.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const RecordSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
+  quantity: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  postingYear: { type: Number, required: true },
+  postingMonth: { type: String, required: true },
+  actionType: { type: String, required: true },
+  actionName: { type: String, required: true },
+  status: { type: String, required: true },
+  Impact: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now }
+});
+
+const Record = mongoose.model('Record', RecordSchema);
+module.exports = Record;
